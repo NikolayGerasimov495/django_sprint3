c121d9e21418c81533969fd696a46163fd500a80
Nikolay Gerasimov <nikolay.gerasimov495@yandex.ru>
2024-03-05 23:01:12 +0300

    add pep8

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -12,9 +12,8 @@ class Category(PublishedModel):
     slug = models.SlugField(
         verbose_name='Идентификатор',
         unique=True,
-        help_text='''Идентификатор страницы для URL;
-                    разрешены символы латиницы,
-                    цифры, дефис и подчёркивание.''')
+        help_text='Идентификатор страницы для URL; разрешены символы латиницы'
+        ', цифры, дефис и подчёркивание.')
 
     class Meta:
         verbose_name = 'категория'
@@ -40,8 +39,8 @@ class Post(PublishedModel):
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text='''Если установить дату и время в будущем —
-                    можно делать отложенные публикации.''')
+        help_text='Если установить дату и время в будущем — можно '
+        'делать отложенные публикации.')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
