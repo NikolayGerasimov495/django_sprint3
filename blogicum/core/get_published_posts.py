4ce03c89e39c7433aecf33e1440e9a5cb1d0c489
Nikolay Gerasimov <nikolay.gerasimov495@yandex.ru>
2024-03-06 22:04:11 +0300

    fix pep8

--- blogicum/core/get_published_posts.py
+++ blogicum/core/get_published_posts.py
@@ -6,5 +6,4 @@ def get_published_posts():
     return Post.objects.select_related('category', 'author').filter(
         pub_date__lte=timezone.now(),
         is_published=True,
-        category__is_published=True
-    )
+        category__is_published=True)
